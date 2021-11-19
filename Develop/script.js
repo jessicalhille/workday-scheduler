ab62ff118a0ab219ba676b395c5913cead1ae8cd
Jessica Hille <jessicalhille@gmail.com>
2021-11-18 21:40:09 -0600

    added missing save icon on html button, started js function

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,9 @@
-// Display today's day and date
+// Display today's day
 var todayDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(todayDate);
 
+$(document).ready(function() {
+    $(".saveBtn").on("click", function() {
+        console.log("success");
+    })
+})
