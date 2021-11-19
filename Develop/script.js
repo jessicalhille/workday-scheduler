f291e80dfc240dafdfcbc08174eb0b4c8a449584
Jessica Hille <jessicalhille@gmail.com>
2021-11-18 22:31:21 -0600

    past present future function added to javascript, localStorage code not working yet

--- Develop/script.js
+++ Develop/script.js
@@ -4,6 +4,36 @@ $("#currentDay").html(todayDate);
 
 $(document).ready(function() {
     $(".saveBtn").on("click", function() {
-        console.log("success");
-    })
+        var time = $(this).parent().attr("id");
+        var text = $(this).siblings(".description").val();
+
+        localStorage.setItem(time, text);
+    });
+
+    function scheduleTime() {
+        // get current time
+        var timeNow = moment().hour();
+
+        $(".time-block").each(function() {
+            var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+            if (blockTime < timeNow) {
+                $(this).removeClass("present");
+                $(this).removeClass("future");
+                $(this).addClass("past");
+            } else if (blockTime === timeNow) {
+                $(this).removeClass("future");
+                $(this).removeClass("past");
+                $(this).addClass("present");
+            } else {
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+                $(this).addClass("future");
+            };
+        });
+    };
+
+    $("hour9 .description").val(localStorage.getItem("hour9"));
+
+    scheduleTime();
 })
